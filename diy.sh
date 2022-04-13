e109bfb20f012b384d44f5ffab13d9dfcd27c245
Jerry <xzpjerry@gmail.com>
2022-04-13 17:45:33 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -3,3 +3,9 @@
 
 # Modify default IP.
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+wget https://github.com/vernesong/OpenClash/archive/master.zip
+unzip master.zip
+cp -r OpenClash-master/luci-app-openclash package/
+./scripts/feeds update -a
+./scripts/feeds install -a
